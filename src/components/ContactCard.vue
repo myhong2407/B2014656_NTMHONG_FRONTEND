470379ff6590c334a61d625dc8026841e43fdb22
abxdcsf <91486810+abxdcsf@users.noreply.github.com>
2023-10-18 19:00:23 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true},
+    },
+};
+</script>
+
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{ contact.name }}
+        </div>
+        <div class="p-1">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Điện thoại:</strong>
+            {{ contact.phone }}
+        </div>
+        <div class="p-1">
+            <strong>Liên hệ yêu thích: &nbsp;</strong>
+            <i v-if="contact.favorite" class="fas fa-check"></i>
+            <i v-else class="fas fa-times"></i>
+        </div>
+    </div>
+</template>
---
+++ src/components/InputSearch.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    props: {
+        modelValue: {type: String, default: ""},
+    },
+    emits: ["submit", "update:modelValue"],
+    methods: {
+        updateModelValue(e) {
+            this.$emit("update:modelValue", e.target.value);
+        },
+        submit() {
+            this.$emit("submit");
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="input-group">
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Nhập thông tin cần tìm"
+            :value="modelValue"
+            @input="updateModelValue"
+            @keyup.enter="submit"
+        />
+        <div class="input-group-append">
+            <button
+                class="btn btn-outline-secondary"
+                type="button"
+                @click="submit"
+            >
+                <i class="fas fa-search"></i> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>
